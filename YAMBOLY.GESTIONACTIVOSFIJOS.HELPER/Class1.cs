c3baf4ea0ff7e080e21c5edc14b2fa0c19de11bc
Gabriela Antezana <gabiantezana@hotmail.com>
2018-09-17 18:01:39 -0500

    Add project files.

---
+++ YAMBOLY.GESTIONACTIVOSFIJOS.HELPER/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAMBOLY.GESTIONACTIVOSFIJOS.HELPER
+{
+    public class Class1
+    {
+    }
+}
---
+++ YAMBOLY.GESTIONACTIVOSFIJOS.TESTER/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAMBOLY.GESTIONACTIVOSFIJOS.TESTER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //YAMBOLY.GESTIONACTIVOSFIJOS.FORM._MSS_APROForm.MSS_APROForm.CreatePicking(12);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+        }
+    }
+}
